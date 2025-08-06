8263279f6a905e758fd7e39c0a2ebc60e1424559
Linh3694 <hailinh.n23@gmail.com>
2025-08-06 15:58:20 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,64 +1,31 @@
 module.exports = {
-  apps: [
-    {
-      name: 'notification-service-1',
-      script: 'app.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development',
-        PORT: 5003,
-        INSTANCE_ID: '1'
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 5003,
-        INSTANCE_ID: '1'
-      },
-      error_file: './logs/err-1.log',
-      out_file: './logs/out-1.log',
-      log_file: './logs/combined-1.log',
-      time: true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      merge_logs: true,
-      max_restarts: 10,
-      min_uptime: '10s',
-      restart_delay: 4000,
-      kill_timeout: 5000,
-      listen_timeout: 8000,
-      shutdown_with_message: true
+  apps: [{
+    name: 'notification-service',
+    script: 'app.js',
+    instances: 2,
+    instance_var: 'INSTANCE_ID',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 5001
     },
-    {
-      name: 'notification-service-2',
-      script: 'app.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      env: {
-        NODE_ENV: 'development',
-        PORT: 5004,
-        INSTANCE_ID: '2'
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        PORT: 5004,
-        INSTANCE_ID: '2'
-      },
-      error_file: './logs/err-2.log',
-      out_file: './logs/out-2.log',
-      log_file: './logs/combined-2.log',
-      time: true,
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
-      merge_logs: true,
-      max_restarts: 10,
-      min_uptime: '10s',
-      restart_delay: 4000,
-      kill_timeout: 5000,
-      listen_timeout: 8000,
-      shutdown_with_message: true
-    }
-  ]
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 5001
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    log_file: './logs/combined.log',
+    time: true,
+    log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    merge_logs: true,
+    max_restarts: 10,
+    min_uptime: '10s',
+    restart_delay: 4000,
+    kill_timeout: 5000,
+    listen_timeout: 8000,
+    shutdown_with_message: true
+  }]
 }; 
